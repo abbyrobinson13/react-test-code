f1b7754daf4c4f5fa061befe9fa32f8a7010222c
Abigail Robinson <abbyrobinson@shaw.ca>
2023-03-10 17:02:02 -0700

    added word by word component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  return <WordByWord />;
+}
+
+function WordByWord() {
+  const [word, setWord] = useState("");
+
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
+
+  const words = word.split(" ");
+  console.log(words);
+
+  let displayWords;
+  if (words.length >= 2) {
+    displayWords = words.slice(0, words.length - 1).map((w) => <p>{w}</p>);
+  } else {
+    displayWords = null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Type something" onChange={handleChange} />
+      <span>{displayWords} </span>
     </div>
   );
 }
